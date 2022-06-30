7a7bde0d795427291904be62aa3705b2de06589e
valuec <9764kim@naver.com>
2022-06-30 21:55:56 +0900

    feat: Add bottom tab navigator with three tabs

---
+++ app/src/main/java/com/example/themostusefulapp/fragment_todo.java
@@ -0,0 +1,25 @@
+package com.example.themostusefulapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class fragment_todo extends Fragment {
+    public fragment_todo() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+}
